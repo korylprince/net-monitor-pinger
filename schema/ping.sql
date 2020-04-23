cd44e366866d90f4931de74f8baf5b3a0a5145d6
Kory Prince <korylprince@gmail.com>
2020-04-23 02:37:17 -0500

    on delete device cascade delete pings

--- schema/ping.sql
+++ schema/ping.sql
@@ -4,7 +4,7 @@ CREATE TABLE ping (
     sent_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     rtt INTEGER,
     PRIMARY KEY (device_id, sent_time),
-    FOREIGN KEY (device_id) REFERENCES device(id)
+    FOREIGN KEY (device_id) REFERENCES device(id) ON DELETE CASCADE
 );
 
 CREATE INDEX ping_ip ON ping (ip);
